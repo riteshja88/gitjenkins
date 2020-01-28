63d109c0f9148226777ba9e90ead9a642d4cfde1
Ritesh Agarwal <ritesh.agarwal@pubmatic.com>
2020-01-28 03:35:31 -0500

    demo

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 all:
+	sleep 30
 	echo "Makefile was triggered"
 	$(CC)  -c hello.c
+	exit 2
 
 clean:
 	rm hello.o
--- hello.c
+++ hello.c
@@ -4,7 +4,8 @@ int nomain()
 {
 	char *c = malloc(100);
 	char *d = malloc(100);
-	char *e = malloc(100);		
+	char *e = malloc(100);
+	char *f = malloc(100);
 	printf("Hello");
 	return 0;
 }
